74c91ba37fb5c6e740302172a68ca5f11a2f43b2
Mr Barker <89406881+abarkerburgate@users.noreply.github.com>
2025-03-20 10:16:49 +0000

    introduced the chatbot

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+botName = "BlurghBot"
+print("Hello, my name is", botName)
+
+humanName = input("What is your name?")
+print("Hello", humanName)
+
+
+
+
+
+
+
+
+
+
+
+
+response = input("How are you today " + humanName + "(good/bad)")
+if response == "good":
+    print("I am pleased you are well!")
+    
+else:
+    print("I am sorry to hear you are not well.")
+    print("Have you been for a walk today?")
+    print("Or maybe read a book?")
+    print("Or have a nice cake?")
+
